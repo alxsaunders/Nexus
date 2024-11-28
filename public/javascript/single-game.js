16ffc7c9f3e12ebd625e4c8330ef0e20c1698dec
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2024-11-28 12:20:01 -0500

    New Twitch Auth Key

--- public/javascript/single-game.js
+++ public/javascript/single-game.js
@@ -1,7 +1,7 @@
 var coversingle = document.querySelector("#gamename");
 var coverspot = document.querySelector('#gamecover');
 
-let myKey = "Bearer buxticjcoos6sb3hwwdkn0lzu88c03";
+let myKey = "Bearer NONONONONO";
 var apiUrlsingle = `https://api.twitch.tv/helix/games?name=${coversingle.innerText}`
 
 function getGameImage() {
